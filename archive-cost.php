d6bcd30a74d0eae7d4323c38579dacab227c04b4
shuwebdesign <littlecub114mt@gmail.cpm>
2021-12-06 07:19:22 +0900

    #2

--- archive-cost.php
+++ archive-cost.php
@@ -6,22 +6,22 @@ $userName = $user->display_name; //ユーザー名
 <?php get_header(); ?>
 
 <?php 
-//     $member_terms = get_terms( 'member', array( 'hide_empty'=>false)); //メンバーターム取得
-//     if(!empty($member_terms)){
-//         foreach($member_terms as $term){
-//             $post_by_term = get_post_by_member_term($term);
-//             $total_current = 0;
-//             while($post_by_term->have_posts()){
-//                 $post_by_term->the_post();
-//                 $price_current = get_post_current_cost();//今月出費
-//                 $total_current += intval($price_current); //今月出費計算
-//             }
-//             echo $term->name;
-//             echo $total_current;
-//         }
-//     }else{
-//         $html = '<a href="<?php echo get_term_link($term->term_id); ?>" class="userName"><?php echo esc_html( $term->name ); ?></a>';
-// }
+    $member_terms = get_terms( 'member', array( 'hide_empty'=>false)); //メンバーターム取得
+    if(!empty($member_terms)){
+        foreach($member_terms as $term){
+            $post_by_term = get_post_by_member_term($term);
+            $total_current = 0;
+            while($post_by_term->have_posts()){
+                $post_by_term->the_post();
+                $price_current = get_post_current_cost();//今月出費
+                $total_current += intval($price_current); //今月出費計算
+            }
+            echo $term->name;
+            echo $total_current;
+        }
+    }else{
+        $html = '<a href="<?php echo get_term_link($term->term_id); ?>" class="userName"><?php echo esc_html( $term->name ); ?></a>';
+}
 ?>
 <main>
     <div class="container">
